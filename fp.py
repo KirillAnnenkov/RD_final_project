ea6a0049064a1b6ee27d81026a11324139f4b285
user <akir@lcon.com.ua>
2021-11-15 00:41:52 +0200

    alpha

--- fp.py
+++ fp.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
 
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.hooks.base_hook import BaseHook
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.exceptions import AirflowFailException
+from hdfs import InsecureClient
+from pyspark.sql import SparkSession
+
+from datetime import datetime, timedelta
+import json
+import os
+import requests
+import logging
+
+import lib_fp
+
