dcb125b59bce6fa1fad6251fe63ea2ff07c98127
Akir <akir@lcon.com.ua>
2021-11-17 21:44:11 +0200

    a2

--- function/libs.py
+++ function/libs.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+# %%
 # from airflow.hooks.postgres_hook import PostgresHook
 from hdfs import InsecureClient
+from pyspark.sql import SparkSession
 
 import logging
 from datetime import datetime, timedelta
@@ -9,13 +11,12 @@ import os
 
 import psycopg2
 
-# import function.get_config as get_config
 import get_config
 
 def db_export_bronze(**kwargs):
     '''Принимает название таблицы из БД, выгружает таблицу в bronze'''
 
-# Настрой доступа к postgress (no airflow)
+    # Настрой доступа к postgress (no airflow)
     pg_creds = {
         'host': '192.168.0.142'
         , 'port': '5432'
@@ -29,58 +30,72 @@ def db_export_bronze(**kwargs):
     logging.info(f'Начало экспорта таблицы в bronze: {table_name}')
 
     # Создаем соеденение с hdfs
-    url_hdfs = get_config.config('./config/config_fp.json',"url_hdfs")
+    url_hdfs = get_config.config('../config/config_fp.json',"url_hdfs")
     client = InsecureClient(f'{url_hdfs}', user='user')
 
     # Создать папку (дата) для выгрузки данных
-    path_bronze = get_config.config('./config/config_fp.json',"path_bronze")
+    path_bronze = get_config.config('../config/config_fp.json',"path_bronze")
     path_hdfs = os.path.join(path_bronze, str(datetime.today().strftime('%Y-%m-%d')))
 
     # Создать папку, если ее нет
     client.makedirs(path_hdfs)
 
-    # # Подключится к базе дaнных, выгрузить в bronze таблицу
-    # with psycopg2.connect(**pg_creds) as pg_connection:
-    #     cursor = pg_connection.cursor()
-    #     with client.write(os.path.join(path_hdfs, f'{kwargs["table_name"]}.csv' )) as csv_file:
-    #         cursor.copy_expert('COPY (SELECT * FROM {0}) TO STDOUT WITH HEADER CSV'.format(kwargs['table_name']), csv_file)
+    # # Подключится к базе дaнных
+    # conn = PostgresHook(postgres_conn_id="postgres_default").get_conn()
+    # cur = conn.cursor()
+
+    # Подключится к базе дaнных, выгрузить в bronze таблицу
+    with psycopg2.connect(**pg_creds) as pg_connection:
+        cursor = pg_connection.cursor()
+        with client.write(os.path.join(path_hdfs, f'{kwargs["table_name"]}.csv'), overwrite=True) as csv_file:
+            cursor.copy_expert('COPY (SELECT * FROM {0}) TO STDOUT WITH HEADER CSV'.format(kwargs['table_name']), csv_file)
+
+    logging.info(f'Успешный экспорт таблицы в bronze: {table_name}')
+
+def db_to_silver(**kwargs):
+    '''Принимает название таблицы из БД, выгружает таблицу в silver'''
+
+    table_name = kwargs['table_name']
+    logging.info(f'Начало экспорта таблицы в silver: {table_name}')
+
+    # Создаем соеденение с hdfs
+    url_hdfs = get_config.config('../config/config_fp.json',"url_hdfs")
+    client = InsecureClient(f'{url_hdfs}', user='user')
+
+   # Получить путь к bronze
+    path_bronze = get_config.config('../config/config_fp.json',"path_bronze")
+    path_bronze_date = os.path.join(path_bronze, str(datetime.today().strftime('%Y-%m-%d')))
+
+    # Создать папку (дата) для выгрузки данных в silver
+    path_silver = get_config.config('../config/config_fp.json',"path_silver")
+    path_silver_date = os.path.join(path_silver, str(datetime.today().strftime('%Y-%m-%d')))
+
+    client.makedirs(path_silver_date)
+
+    spark = SparkSession \
+        .builder.master('local') \
+        .appName('FP') \
+        .getOrCreate()
+
+    df = spark.read.load(os.path.join(path_bronze_date, table_name + '.csv')
+                         , header="true"
+                         , inferSchema="true"
+                         , format="csv")
+
+    df = df.drop_duplicates()
+
+    df.write.parquet(os.path.join(path_silver_date, table_name), mode='overwrite')
+
+    logging.info(f'Успешный экспорт таблицы в silver: {table_name}')
 
 
-    # Создать папку (дата) для выгрузки данных
-    path_hdfs = os.path.join(
-        '/', 'hm', 'homework7', 'bronze', 'db', str(datetime.today().strftime('%Y-%m-%d')))
 
-    client.makedirs(path_hdfs)
+if __name__ == "__main__":
+    # Получить список таблиц для выгрузки из конфига
+    tables = get_config.config('../config/config_fp.json',"db_tables")
 
+    for table in tables:
+        db_export_bronze(table_name=table)
+        db_to_silver(table_name=table)
 
-    # with psycopg2.connect(**pg_creds) as pg_connection:
-    #     cursor = pg_connection.cursor()        
-    #     with open(file=table_name+'.csv', mode='w') as csv_file:            
-    #         cursor.copy_expert('COPY (SELECT * FROM {0}) TO STDOUT WITH HEADER CSV'.format(kwargs['table_name']), csv_file)
-
-
-    import pandas as pd
-    # df = pd.read_csv("aisles.csv")
-
-    # with client.write(os.path.join(path_hdfs, f'{kwargs["table_name"]}.csv'), encoding = 'utf-8') as writer:
-    #     df.to_csv(writer)
-
-    # Creating a simple Pandas DataFrame
-    liste_hello = ['hello1','hello2']
-    liste_world = ['world1','world2']
-    df = pd.DataFrame(data = {'hello' : liste_hello, 'world': liste_world})
-    
-    # # Writing Dataframe to hdfs
-    # with client.write('/user/hdfs/wiki/helloworld.csv', encoding = 'utf-8') as writer:
-    #     pass
-    #     # df.to_csv(writer)
-
-
-    # # # # Подключится к базе дaнных
-    # # # conn = PostgresHook(postgres_conn_id="postgres_default").get_conn()
-    # # # cur = conn.cursor()
-
-    # logging.info(f'Успешный экспорт таблицы в bronze: {table_name}')
-
-
-db_export_bronze(table_name='aisles')
+# %%
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # %%
 import function.get_config as get_config
 
-import function.libs as libs
+# import function.libs as libs
 
 # Получить список таблиц для выгрузки из конфига
 tables = get_config.config('./config/config_fp.json',"db_tables")
@@ -10,6 +10,6 @@ tables = get_config.config('./config/config_fp.json',"db_tables")
 
 for table in tables[0:1]:
     libs.db_export_bronze(table_name = table)
-    print(table)
+    print(table='aisles')
 
 # %%
